e6df77e7e51d51d8807ba9b7b7d83e7aee3ae0dc
Phil Kemp <boondock.dragon@gmail.com>
2024-07-18 11:16:57 +0200

    ROU / Colentina Bucuresti

--- esl-script.js
+++ esl-script.js
@@ -18,7 +18,7 @@ const countries = {
 	"swe": "Sweden"
 };
 
-clubPages = ["preston_north_end_fc","akademisk_boldklub","kjobenhavns_boldklub","linfield_fc","rangers_fc","budapesti_tc","royale_union_saint_gilloise","kfc_rhodienne_de_hoek","floriana_fc"];
+clubPages = ["preston_north_end_fc","akademisk_boldklub","kjobenhavns_boldklub","linfield_fc","rangers_fc","budapesti_tc","royale_union_saint_gilloise","kfc_rhodienne_de_hoek","floriana_fc","colentina_ac_bucuresti"];
 clubPages = [];
 
 if ( $("h1 .placeholder").length !== 0 ) {
