3fd1dfaa8223e7de915e3fa6ff051f05e9b94a9a
Phil Kemp <boondock.dragon@gmail.com>
2024-08-09 17:39:51 +0200

    CSK / Sparta Prague

--- esl-script.js
+++ esl-script.js
@@ -19,8 +19,8 @@ const countries = {
 	"swe": "Sweden"
 };
 
-clubPages = ["preston_north_end_fc","akademisk_boldklub","kjobenhavns_boldklub","linfield_fc","rangers_fc","budapesti_tc","royale_union_saint_gilloise","kfc_rhodienne_de_hoek","floriana_fc","colentina_ac_bucuresti","wiener_ac","knattspyrnufelagio_fram","us_hollerich_bonnevoie","mtk_budapest_fc","knattspyrnufelagio_vikingur","sliema_wanderers_fc"];
-//clubPages = [];
+clubPages = ["preston_north_end_fc","akademisk_boldklub","kjobenhavns_boldklub","linfield_fc","rangers_fc","budapesti_tc","royale_union_saint_gilloise","kfc_rhodienne_de_hoek","floriana_fc","colentina_ac_bucuresti","wiener_ac","knattspyrnufelagio_fram","us_hollerich_bonnevoie","mtk_budapest_fc","knattspyrnufelagio_vikingur","sliema_wanderers_fc","ac_sparta_prague"];
+clubPages = ["ac_sparta_prague"];
 
 if ( $("h1 .placeholder").length !== 0 ) {
 
