e39311f442d035800e9fdd53c00d39741316fc6b
Phil Kemp <boondock.dragon@gmail.com>
2024-08-22 16:50:49 +0200

    Portugal

--- extract.js
+++ extract.js
@@ -1,4 +1,4 @@
-country = "cyp";
+country = "prt";
 current_season = false;
 teams = [];
 ADD_TABLE_STATS = [false];
@@ -7,11 +7,11 @@ FOUND = false;
 tbl = $("#League, #Final_table, #Final_Table, #League_table, #League_Table, #League_standings, #Final_league_table, #Preliminary_stage, #Table");
 if ( tbl.length === 1 ) { FOUND = true; }
 
+/*
 tbl2 = $("#First_round, #Group_A, #Group_B, #Group_C");
 if ( tbl2.length === 4 ) { FOUND = true; ADD_TABLE_STATS = [false,false,false]; console.warn("ABC"); tbl=tbl2;}
 tbl2 = $("#League_table, #Championship_round, #Relegation_round");
 if ( tbl2.length === 3 ) { FOUND = true; ADD_TABLE_STATS = [false,false]; console.warn("Champ+Rel"); tbl=tbl2;}
-/*
 tbl2 = $("#Preliminary_stage, #Championship_playfoo");
 if ( tbl2.length === 4 ) { FOUND = true; ADD_TABLE_STATS = [false,false,false]; console.warn("G123"); tbl=tbl2;}
 */
