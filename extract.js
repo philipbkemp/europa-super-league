5cb5e5e25b9c40628e1c463d4672355905898e20
Phil Kemp <boondock.dragon@gmail.com>
2024-09-11 17:24:39 +0200

    Slovenia

--- extract.js
+++ extract.js
@@ -1,4 +1,4 @@
-country = "rus";
+country = "svn";
 current_season = false;
 teams = [];
 ADD_TABLE_STATS = [false];
@@ -7,7 +7,7 @@ FOUND = false;
 tbl = $("#League_table, #League, #Final_table, #Final_Table, #League_standings, #Final_league_table, #Preliminary_stage, #Standings, #Final_standings, #Regular_season");
 if ( tbl.length === 1 ) { FOUND = true; }
 
-tbl2 = $("#League_table, #Championship_group_table, #Relegation_group_table");
+tbl2 = $("#First_stage, #Championship_group, #Relegation_group");
 if ( tbl2.length === 3 ) { FOUND = true; ADD_TABLE_STATS = [false,false]; console.warn("C/R"); tbl=tbl2;}
 /*
 tbl2 = $("#Second_stage");
